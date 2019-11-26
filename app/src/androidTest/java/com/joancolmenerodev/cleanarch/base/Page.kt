d2e20bed2ecdf4eb02a293be09b2975cc3e58479
Joan Colmenero <joancolmenero@gmail.com>
2019-11-26 14:39:09 +0100

    removing unused method

--- app/src/androidTest/java/com/joancolmenerodev/cleanarch/base/Page.kt
+++ app/src/androidTest/java/com/joancolmenerodev/cleanarch/base/Page.kt
@@ -1,13 +1,9 @@
 package com.joancolmenerodev.cleanarch.base
 
 import android.content.Context
-import androidx.annotation.StringRes
 
-abstract class Page (private val context: Context) {
+abstract class Page(private val context: Context) {
 
     abstract fun at()
 
-    fun getString(@StringRes id: Int): String {
-        return context.getString(id)
-    }
 }
